8c6a9d6a8bc88bfb5ab50151327d617e224f111b
unknown <emprenderusinque@hotmail.com>
2023-02-27 12:12:55 -0500

    creando modal manualmente

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,6 +1,7 @@
 
 import { renderAddButtons } from "./presentation/render-add-buttons/render-add-button";
 import { renderButtons } from "./presentation/render-buttons/render-buttons";
+import { renderModal } from "./presentation/render-modal/render-modal";
 import { renderTable } from "./presentation/render-table/render-table";
 import usersStore from "./store/users-store";
 
@@ -13,6 +14,7 @@ export const UsersApp = async( element ) => {
 
     renderTable( element );
     renderButtons( element );
-    renderAddButtons(element)
+    renderAddButtons(element);
+    renderModal( element );
 
 } 
